3fc0c86707beb7993ac628b4153318ca435eee7c
t179a <67852630+t179a@users.noreply.github.com>
2020-12-08 19:18:58 +0900

    Create a.cpp

---
+++ ABC/183/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  int ans;
+  if(N >= 0){
+    ans = N;
+  }else {
+    ans = 0;
+  }
+  
+  cout << ans << endl;
+}

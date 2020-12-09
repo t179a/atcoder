f53f33c1c939d4d5532219572069f31562aa9b12
t179a <67852630+t179a@users.noreply.github.com>
2020-12-09 17:34:25 +0900

    Create a.cpp

---
+++ ABC/171/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  char c;
+  cin >> c;
+  char ans;
+  if(isupper(c)){
+    ans = 'A';
+  }else{
+    ans = 'a';
+  }
+  cout << ans << endl;
+}

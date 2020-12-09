02d271ce971f82b976301f018d2ec1f5f765debd
t179a <67852630+t179a@users.noreply.github.com>
2020-12-09 11:01:57 +0900

    Create a.cpp

---
+++ ABC/177/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double d, t, s;
+  cin >> d >> t >> s;
+  
+  if(t >= d/s){
+    cout << "Yes" << endl;
+  }else{
+    cout << "No" << endl;
+  }
+  return 0;
+}
+  

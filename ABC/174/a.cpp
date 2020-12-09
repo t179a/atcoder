7c97ae6cb11107e53f59cd3bf06893116bbd41fe
t179a <67852630+t179a@users.noreply.github.com>
2020-12-09 15:41:39 +0900

    Create a.cpp

---
+++ ABC/174/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  string ans;
+  if(N >= 30) {
+    ans = "Yes";
+  }else {
+    ans = "No";
+  }
+  
+  cout << ans << endl;
+}

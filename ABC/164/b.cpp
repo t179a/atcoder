368eb7e4bd0a03c022c07cea29344c7ea9487072
t179a <67852630+t179a@users.noreply.github.com>
2020-12-10 20:02:26 +0900

    Create b.cpp

---
+++ ABC/164/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  
+  while(a > 0 && c > 0){
+    c -= b;
+    if(c <= 0){
+      cout << "Yes" << endl;
+      return 0;
+    }
+    a -= d;
+    if(a <= 0) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+}
+    

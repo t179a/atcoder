36cd1413d11e6049fc39f629ca538bfac62055c6
t179a <67852630+t179a@users.noreply.github.com>
2020-12-11 18:11:44 +0900

    Create b.cpp

---
+++ ABC/154/b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  int num = s.size();
+  string ans = "";
+  for(int i = 0; i < num; i++){
+	ans += "x";
+  }
+  cout << ans << endl;
+}

314b7bde8a344b738f8759f2fdc16bb11c85a6fc
t179a <67852630+t179a@users.noreply.github.com>
2020-12-17 19:06:25 +0900

    Create a.cpp

---
+++ ABC/138/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a;
+  string s;
+  cin >> a >> s;
+  if(a < 3200){
+    cout << "red" << endl;
+    return 0;
+  }else{
+    cout << s << endl;
+    return 0;
+  }
+}

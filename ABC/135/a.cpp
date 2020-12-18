9e89efd8504f51ea4df2a368ad8a6efdd015f640
t179a <67852630+t179a@users.noreply.github.com>
2020-12-18 18:37:18 +0900

    Create a.cpp

---
+++ ABC/135/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int A, B;
+  cin >> A >> B;
+  int sum = A + B;
+  if((A + B)%2 == 1){
+    cout << "IMPOSSIBLE" << endl;
+  }else{
+    cout << (A+B)/2 << endl;
+  }
+}

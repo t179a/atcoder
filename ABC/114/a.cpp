0e4ac6989c853cfb22155bb90eba124111549009
t179a <67852630+t179a@users.noreply.github.com>
2020-12-22 06:08:56 +0900

    Create a.cpp

---
+++ ABC/114/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int X;
+  cin >> X;
+  string ans = "NO";
+  if(X == 3 || X == 5 || X == 7){
+    ans = "YES";
+  }
+  cout << ans << endl;
+}

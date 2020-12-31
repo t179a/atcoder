1f63d07e8f21e58640f670148893f0ecf019ca83
t179a <67852630+t179a@users.noreply.github.com>
2020-12-31 12:40:16 +0900

    Create a.cpp

---
+++ ABC/003/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N;
+  cin >> N;
+  int sum = 0;
+  for(int i = 1; i <= N; i++){
+    sum += i * 10000;
+  }
+  int ans = sum / N;
+  cout << ans << endl;
+}

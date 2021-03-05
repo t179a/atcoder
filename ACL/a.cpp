71c14782ceae9948c3e6cd83888e5da202471a2a
t179a <67852630+t179a@users.noreply.github.com>
2021-03-05 22:48:14 +0900

    Create a.cpp

---
+++ ACL/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int K; cin >> K;
+  string ans = "";
+  for(int i = 0; i < K; i++){
+    ans += "ACL";
+  }
+  cout << ans << endl;
+}

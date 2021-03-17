fc7ad88f4a9c6c7f8b9e7bf3a963cb05aaa11152
t179a <67852630+t179a@users.noreply.github.com>
2021-03-17 22:58:35 +0900

    Create a.cpp

---
+++ エイシンプログラミングコンテスト/a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int L, R, d; cin >> L >> R >> d;
+  
+  int ans = 0;
+  for(int i = L; i <= R; i++){
+    if(i % d == 0){
+      ans++;
+    }
+  }
+  cout << ans << endl;
+}

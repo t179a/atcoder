c585b79de62a5c670429172055e0da8efb25ee9b
t179a <67852630+t179a@users.noreply.github.com>
2021-03-24 19:44:23 +0900

    Create a.cpp

---
+++ パナソニック2020/a.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int K; cin >> K;
+  vector<int> N = {1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51
+};
+  cout << N.at(K-1) << endl;
+}
+  

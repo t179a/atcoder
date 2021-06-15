38956f26579319d8c57e090455154022f4fa8ee3
t179a <67852630+t179a@users.noreply.github.com>
2021-06-15 18:55:51 +0900

    Create a.cpp

---
+++ Indeedなう（予選A）/a.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string A, B; cin >> A >> B;
+  int len_A = A.size();
+  int len_B = B.size();
+  int ans = len_A * len_B;
+  cout << ans << endl;
+}
